03a95ec3db7a5cfeae0767ee99d0ffc86a7881c0
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-08-16 22:38:50 +0530

    test 1

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("hello Chanidu");
+    res.send("hello Chanidu Karunarathna");
 })
 
 const port = 3000;
