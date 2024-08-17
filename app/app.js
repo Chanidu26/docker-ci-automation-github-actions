9d99e3ed8f912abb98f8d37100bd17a078dcf63e
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-08-17 23:36:41 +0530

    test2

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("hello Chanidu Karunarathna");
+    res.send("Docker is Awesome");
 })
 
 const port = 3000;
